a87e9d66bbaa38ba7dc0dd159eef3905bde7446e
surajrai-code <surajraieng>
2023-06-15 23:40:24 +0530

    2nd task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from './components/LayOut/Header'
-
+import MealsSummary from './components/Meal/MealsSummary.js'
 const App = () => {
   return (
     <div>
       <Header/>
+      <MealsSummary/>
     </div>
   )
 }
