24fd94f020c1693be0ad46be5cfdd9661c5d1a34
surajrai-code <surajraieng>
2023-06-16 09:43:56 +0530

    4 th task

--- src/components/Meal/Mealitem.js
+++ src/components/Meal/Mealitem.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import classes from './Mealitem.module.css';
+import MealForm from './MealForm';
 const Mealitem = (props) => {
   return (
     <li className={classes.meal}>
     <div><h3>{props.name}</h3>
     <div className={classes.description}>{props.description}</div>
     <div className={classes.price}>{props.price}</div>
+    </div>
+    <div>
+      <MealForm id={props.id}/>
     </div>
     </li>
   )
